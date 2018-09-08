8318c3d511a221f11c086cc41b63c98d1d4e1d26
catonsun <caton.sun@gmail.com>
2018-09-08 02:09:37 -0500

    created upload funcitonality, added templates and picture upload folder

--- app.py
+++ app.py
@@ -1,10 +1,19 @@
-from flask import Flask
+from flask import Flask, render_template, request, redirect, url_for
+import os
 app = Flask(__name__)
 
 @app.route("/")
-def hello():
-    return "Hello World!"
+def index():
+    return render_template("home.html")
 
-@app.route("/upload")
+@app.route("/upload", methods=['POST', 'GET'])
 def upload():
-    return "This is our upload page"
+    if request.method == 'POST':
+        file = request.files['file']
+        filename = file.filename
+        file.save(os.path.join('UPLOAD_FOLDER', filename))
+        return redirect(url_for('index'))
+    return render_template("upload.html")
+
+if __name__ == '__main__':
+    app.run()
