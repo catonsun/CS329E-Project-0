e5385bd0c9ed2e961aa788d006309fab28491c2a
Kevin Zhang <kevinzhang@utexas.edu>
2018-09-13 11:49:13 -0500

    allowed for login
    ugly hard coded login

--- app.py
+++ app.py
@@ -19,5 +19,10 @@ def upload():
 def login():
     return render_template("login.html")
 
+@app.route("/submit")
+def submit():
+    return render_template("submit.html")
+
+
 if __name__ == '__main__':
     app.run()
