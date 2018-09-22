19ce656973d7ea778eb69476382bab782c2bb25c
catonsun <caton.sun@gmail.com>
2018-09-22 03:14:33 -0500

    changed admin access on home page, added message board for admins only

--- home.php
+++ home.php
@@ -1,18 +1,6 @@
 <!DOCTYPE html>
 
-
 <html>
-<script>
-
-    var obj1 = localStorage.user;
-    console.log(obj1);
-
-    var obj2 = localStorage.password;
-    console.log(obj2);
-
-
-
-</script>
     <head>
         <meta charset="utf-8">
         <link rel="stylesheet" href="home.css">
@@ -24,14 +12,33 @@
         <center>
         <h1>Welcome to our Pet Gallery!</h1>
         <br>
-        <p>This is a simple gallery for pet lovers to upload pictures of their lovely pets! <br> We hope you enjoy!<br\></p>
+        <p>This is a simple gallery for pet lovers to upload pictures of their lovely pets! <br> We hope you enjoy!</p>
         <p>
-        <a href="login.html">Click here to login</a>
-        <br>
-        <a href="upload.html">Click here to upload</a>
-        <br>
-        <a href="rules.html">Click here to view rules</a>
-        <br>
+            <a href="login.html">Click here to login</a><br>
+            <a href="rules.html">Click here to view rules</a><br>
+            <a href="upload.html" id="upload">Click here to upload</a><br>
+            <a href="messages.html" id="messages">Click here to post a message (admin only)</a><br>
+            <script>
+                if (localStorage.getItem("accessLevel") == -1) {
+                    var x = document.getElementById("upload");
+                    x.style.display = "none";
+                    x = document.getElementById("messages");
+                    x.style.display = "none";
+
+                }
+                else if (localStorage.getItem("accessLevel") == 0) {
+                    var x = document.getElementById("upload");
+                    x.style.display = "block";
+                    x = document.getElementById("messages");
+                    x.style.display = "none";
+                }
+                else if (localStorage.getItem("accessLevel") == 1) {
+                    var x = document.getElementById("upload");
+                    x.style.display = "block";
+                    x = document.getElementById("messages");
+                    x.style.display = "block";
+                }
+            </script>
         </p>
         <?php
             $dirname = "UPLOAD_FOLDER/";
